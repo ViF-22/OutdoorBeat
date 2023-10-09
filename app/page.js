b6d9e715875ad71b86a4f93da88a571c2b93e8b7
ViF-22 <feofanovaviktoriya@gmail.com>
2023-10-09 16:16:17 +0300

    smooth scroll addition

--- app/page.js
+++ app/page.js
@@ -1,11 +1,25 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
 import Hero from "./components/Hero";
 import TextBlock from "./components/TextBlock";
 import FullWidthImage from "./components/FullWidthImage";
 import JoinUs from "./components/JoinUs";
+import Lenis from "@studio-freight/lenis";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
     <div>
       <Hero />
